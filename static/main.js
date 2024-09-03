71664dc68c6d58c69862adf5dda004905d7d4907
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-09-03 20:40:09 +0300

    add background color to popups, refactor

--- static/main.js
+++ static/main.js
@@ -63,15 +63,15 @@ function randomizeSvgIdsHandleElement(elm, idsMap) {
 }
 // from https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
 function randomString(length) {
-    let result = '';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const charactersLength = characters.length;
-    let counter = 0;
-    while (counter < length) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-      counter += 1;
-    }
-    return result;
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
 }
 
 let popupElm;
