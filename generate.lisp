07d3fc2876fde12b4bc86353046f8847697ca0b7
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2025-08-14 16:06:59 +0300

    properly copy latex previews

--- generate.lisp
+++ generate.lisp
@@ -15,20 +15,22 @@
 (defvar *my-metadata* nil)
 
 (defun generate ()
-  (let* ((template-dir (truename "~/work/template/"))
-         (other-head-contents
-           (uiop:read-file-string
-            (uiop:merge-pathnames* template-dir "head.html")))
-         (other-preamble-contents
-           (uiop:read-file-string
-            (uiop:merge-pathnames* template-dir "preamble.html")))
-         (cltpt/html:*html-static-route* "/")
-         (*my-metadata*
-           (list :other-head-contents other-head-contents
-                 :other-preamble-contents other-preamble-contents))
-         (cltpt/html:*html-postamble* "</body></html>")
-         (cltpt/html:*html-preamble*
-           "<html>
+  (uiop:with-current-directory ((truename "~/work/blog/"))
+    (let* ((template-dir (truename "~/work/template/"))
+           (other-head-contents
+             (uiop:read-file-string
+              (uiop:merge-pathnames* template-dir "head.html")))
+           (other-preamble-contents
+             (uiop:read-file-string
+              (uiop:merge-pathnames* template-dir "preamble.html")))
+           (cltpt/html:*html-static-route* "/")
+           (cltpt/latex:*latex-previews-cache-directory* #P"./")
+           (*my-metadata*
+             (list :other-head-contents other-head-contents
+                   :other-preamble-contents other-preamble-contents))
+           (cltpt/html:*html-postamble* "</body></html>")
+           (cltpt/html:*html-preamble*
+             "<html>
 <head>
   <title> %title </title>
   #(getf cl-user::*my-metadata* :other-head-contents)
@@ -36,28 +38,27 @@
 <body>
   #(getf cl-user::*my-metadata* :other-preamble-contents)
 ")
-         (rmr (cltpt/roam:from-files
-               '((:path ("/home/mahmooz/brain/notes/")
-                  :regex ".*\\.org"
-                  :format "org-mode")))))
-    (cltpt/roam:convert-all
-     rmr
-     (cltpt/base:text-format-by-name "html")
-     "/home/mahmooz/work/blog/%(cl-user::title-to-filename title).html")
-    (mapc
-     (lambda (item)
-       (uiop:copy-file (uiop:merge-pathnames* template-dir item)
-                       (uiop:merge-pathnames* (truename "~/work/blog/")
-                                              item)))
-     (list "head.html" "preamble.html" "search.html"))
-    (cltpt/base:ensure-directory "/home/mahmooz/work/blog/static/")
-    (mapc
-     (lambda (item)
-       (format t "hey ~A~%" item)
-       (uiop:copy-file (uiop:merge-pathnames* (truename "~/work/template/static/")
-                                              item)
-                       (uiop:merge-pathnames* (truename "~/work/blog/static/")
-                                              item)))
-     (uiop:directory-files (truename "~/work/template/static/")))))
+           (rmr (cltpt/roam:from-files
+                 '((:path ("/home/mahmooz/brain/notes/")
+                    :regex ".*\\.org"
+                    :format "org-mode")))))
+      (cltpt/roam:convert-all
+       rmr
+       (cltpt/base:text-format-by-name "html")
+       "/home/mahmooz/work/blog/%(cl-user::title-to-filename title).html")
+      (mapc
+       (lambda (item)
+         (uiop:copy-file (uiop:merge-pathnames* template-dir item)
+                         (uiop:merge-pathnames* (truename "~/work/blog/")
+                                                item)))
+       (list "head.html" "preamble.html" "search.html"))
+      (cltpt/base:ensure-directory "/home/mahmooz/work/blog/static/")
+      (mapc
+       (lambda (item)
+         (uiop:copy-file (uiop:merge-pathnames* (truename "~/work/template/static/")
+                                                item)
+                         (uiop:merge-pathnames* (truename "~/work/blog/static/")
+                                                item)))
+       (uiop:directory-files (truename "~/work/template/static/"))))))
 
 ;; (generate)
