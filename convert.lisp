587d1f3534ce0c5c63c38851b4ad0e66fd65ea32
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2025-08-30 00:13:25 +0300

    slight fix

--- convert.lisp
+++ convert.lisp
@@ -21,7 +21,7 @@
                 (when (or (typep obj 'cltpt/latex:inline-math)
                           (typep obj 'cltpt/latex:display-math)
                           (typep obj 'cltpt/latex:latex-env))
-                  (push (cltpt/base:text-object-contents obj) all-snippets)))))
+                  (pushnew (cltpt/base:text-object-contents obj) all-snippets :test 'string=)))))
     (loop for i from 0 to (length all-snippets) by snippets-at-once
           do (cltpt/latex:generate-previews-for-latex
               (subseq all-snippets
