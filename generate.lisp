64aa454ce747c9681ba43b9747f7c83145d08fc7
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2026-03-07 20:37:02 +0200

    add commented out code to reload libs locally

--- generate.lisp
+++ generate.lisp
@@ -1,4 +1,11 @@
 #!/usr/bin/env -S sbcl --script
 (require 'asdf)
+;; (pushnew ""
+;;          asdf:*central-registry*
+;;          :test #'equal)
+;; (pushnew (concatenate 'string (uiop:getenv "WORK_DIR") "/cltpt/")
+;;          asdf:*central-registry*
+;;          :test #'equal)
+;; (asdf:load-system :cltpt :force t)
 (asdf:load-system :template)
 (generate)
